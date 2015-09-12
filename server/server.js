2575e229985a240c36b5031e0739d2383208ccf9
Iivari Äikäs <iivari.aikas@gmail.com>
2015-09-12 14:35:58 +0300

    Add eslint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,6 @@ import express from 'express'
 const app = express()
 app.use(express.static(path.join(__dirname, '../dist')))
 
-const server = app.listen(31337, () => {
+app.listen(31337, () => {
   console.log('server starter, port 31337')
 })
