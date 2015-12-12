46c94f58d8a3df00ffc6565050b441c454cb987a
Iivari Äikäs <iivari.aikas@gmail.com>
2015-12-12 23:46:12 +0200

    Move chat line into immutable component

--- client/messages.jsx
+++ client/messages.jsx
@@ -22,7 +22,9 @@ export default React.createClass({
   },
 
   render: function() {
-    const messages = R.map(this.createLineDOM, this.state.lines)
+    const messages = R.map(line =>
+      <Line key={line.key} time={line.time} nick={line.user} msg={line.msg}/>,
+      this.state.lines)
 
     return (
       <div id='messages-wrapper'>
@@ -30,25 +32,32 @@ export default React.createClass({
         <MessageInput sock={this.props.sock}></MessageInput>
       </div>
     )
-  },
-
-  createLineDOM: function(line) {
-    return (
-      <li key={line.key} className='line'>
-        <span className='time'>{this.formatTimestamp(line.time)}</span>
-        <span className='nick'>{line.user}:</span>
-        <span className='msg'>{line.msg}</span>
-      </li>
-    )
-  },
-
-  formatTimestamp: function(datestr) {
-    const date = new Date(datestr)
-    const hours = date.getHours()
-    const mins = date.getMinutes()
-    const hourStr = (hours < 10) ? '0' + hours : hours
-    const minStr = (mins < 10) ? '0' + mins : mins
-
-    return hourStr + ':' + minStr
   }
 })
+
+const Line = React.createClass({
+  shouldComponentUpdate: function() {
+    return false
+  },
+
+  render: function() {
+    return (
+      <li key={this.props.key} className='line'>
+        <span className='time'>{formatTimestamp(this.props.time)}</span>
+        <span className='nick'>{this.props.nick}:</span>
+        <span className='msg'>{this.props.msg}</span>
+      </li>
+    )
+  }
+})
+
+
+function formatTimestamp(datestr) {
+  const date = new Date(datestr)
+  const hours = date.getHours()
+  const mins = date.getMinutes()
+  const hourStr = (hours < 10) ? '0' + hours : hours
+  const minStr = (mins < 10) ? '0' + mins : mins
+
+  return hourStr + ':' + minStr
+}
