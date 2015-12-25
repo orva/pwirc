4aba0950e1c2d27443cc71afc49a7fed4a67fa1b
Iivari Äikäs <iivari.aikas@gmail.com>
2015-12-25 23:41:30 +0200

    Fix typos

--- test/test_routes.js
+++ test/test_routes.js
@@ -15,28 +15,28 @@ describe('POST /messages/:server/:to', function() {
     this.app = proxyquire('../server/http_handlers', { './irc_server': ircStub })
   })
 
-  it('reponds with 200 when both server and target supplied', function(done) {
+  it('responds with 200 server, target and body are supplied', function(done) {
     request(this.app)
       .post('/messages/freenode/%23first')
       .send({ msg: 'test' })
       .expect(200, done)
   })
 
-  it('reponds with 404 when not both server and target are present', function(done) {
+  it('responds with 404 when both server and target are not present', function(done) {
     request(this.app)
       .post('/messages/freenode')
       .send({ msg: 'test' })
       .expect(404, done)
   })
 
-  it('reponds with 404 with empty body', function(done) {
+  it('responds with 404 to empty body', function(done) {
     request(this.app)
       .post('/messages/freenode/%23first')
       .send('')
       .expect(404, done)
   })
 
-  it('reponds with 404 body does not contain "msg" field', function(done) {
+  it('responds with 404 when body does not contain "msg" field', function(done) {
     request(this.app)
       .post('/messages/freenode/%23first')
       .send({ fails: true })
@@ -45,4 +45,5 @@ describe('POST /messages/:server/:to', function() {
 
 
   it('responds with 404 when channel does not exist')
+  it('responds with 404 when server does not exist')
 })
