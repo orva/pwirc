c8e0a5fec900a2af9a4a879d6097c0a50c488ac2
Iivari Äikäs <iivari.aikas@gmail.com>
2015-12-28 16:31:52 +0200

    Move DOM elements into components subdirectory

--- client/client_entry.js
+++ client/client_entry.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './app.jsx'
+import App from './components/app.jsx'
 
 const sock = io.connect()
 const app = React.createElement(App, {sock: sock})
