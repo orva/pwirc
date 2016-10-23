6cc1d45c27e4f9e1358b2bdd3189368ab0e49f11
Iivari Äikäs <iivari.aikas@gmail.com>
2016-10-23 17:20:45 +0300

    Render messages

--- client/client_entry.js
+++ client/client_entry.js
@@ -1,6 +1,8 @@
 const io = require('socket.io-client')
-// const React = require('karet')
 const Atom = require('kefir.atom').default
+const React = require('karet').default  // eslint-disable-line no-unused-vars
+const ReactDOM = require('react-dom')
+const K = require('karet.util').default
 const L = require('partial.lenses')
 const R = require('ramda')
 
@@ -12,22 +14,30 @@ const root = Atom({
 
 root.log('root')
 
-const messages = root.lens(
-  'messages',
-  L.required([]))
+const messages = root.lens('messages')
 const channels = root.lens(
   'channels',
-  L.required([]),
   L.normalize(R.sortBy(R.prop('channel'))))
-const currentChannel = root.lens(
-  'currentChannel',
-  L.required({}))
+const currentChannel = root.lens('currentChannel')
+
 
 const sock = io.connect()
-sock.on('channel-switched', chan => {
-  messages.remove()
-  currentChannel.set(chan)
+sock.on('channel-switched', ({lines, server, channel}) => {
+  messages.set(lines)
+  currentChannel.set({ server, channel })
 })
 sock.on('channels-updated', chans => channels.set(chans))
 sock.on('message', msg => messages.modify(R.append(msg)))
 sock.on('welcome', () => console.log('welcome'))
+
+
+const Messages = ({msgs}) => // eslint-disable-line no-unused-vars
+  <ul>
+   { K(msgs, R.map(({key, user, msg}) => <li key={key}>{user} {msg}</li>)) }
+  </ul>
+
+
+ReactDOM.render(
+  <Messages msgs={messages} />,
+  document.getElementById('messages'))
+
